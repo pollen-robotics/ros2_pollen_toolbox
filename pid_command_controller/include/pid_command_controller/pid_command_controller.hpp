3a8bf23f2f74572c9ea3f0f285ae9513efc13b98
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2025-09-17 11:27:06 +0200

    revert

--- pid_command_controller/include/pid_command_controller/pid_command_controller.hpp
+++ pid_command_controller/include/pid_command_controller/pid_command_controller.hpp
@@ -24,7 +24,7 @@
 #include "rclcpp/subscription.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "realtime_tools/realtime_buffer.hpp"
+#include "realtime_tools/realtime_buffer.h"
 #include "std_msgs/msg/float64_multi_array.hpp"
 
 namespace pid_command_controller
