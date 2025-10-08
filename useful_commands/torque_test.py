5b9dd55743b4391a93a747ae0acb1383b6d4b4eb
glannuzel <gaelle.lannuzel@gmail.com>
2025-10-08 18:10:50 +0200

    Update torque_test.py

--- useful_commands/torque_test.py
+++ useful_commands/torque_test.py
@@ -5,6 +5,8 @@ import time
 if __name__ == "__main__":
     print("Reachy 2 test: torques")
 
+    test_result = "SUCCEEDED"
+
     logging.basicConfig(level=logging.INFO)
     reachy = ReachySDK(host="localhost")
 
@@ -27,11 +29,11 @@ if __name__ == "__main__":
             reachy.l_arm.goto_posture()
             reachy.r_arm.gripper.open()
             reachy.l_arm.gripper.open()
-            head_goto = reachy.head.look_at(0.5, 10, 0)
-            reachy.head.l_antenna.goto_position(-10)
-            reachy.head.r_antenna.goto_position(10)
+            head_goto = reachy.head.look_at(0.5, 0, 10)
+            reachy.head.l_antenna.goto(-20)
+            reachy.head.r_antenna.goto(20)
 
-            while not reachy.head.is_goto_finished(head_goto):
+            while not reachy.is_goto_finished(head_goto):
                 time.sleep(0.1)
             time.sleep(0.5)
 
@@ -39,20 +41,21 @@ if __name__ == "__main__":
             reachy.l_arm.goto_posture("elbow_90")
             reachy.r_arm.gripper.close()
             reachy.l_arm.gripper.close()
-            head_goto = reachy.head.look_at(0.5, -20, 0)
-            reachy.head.l_antenna.goto_position(30)
-            reachy.head.r_antenna.goto_position(-30)
+            head_goto = reachy.head.look_at(0.5, 0, -20)
+            reachy.head.l_antenna.goto(50)
+            reachy.head.r_antenna.goto(-50)
 
-            while not reachy.head.is_goto_finished(head_goto):
+            while not reachy.is_goto_finished(head_goto):
                 time.sleep(0.1)
             time.sleep(0.5)
 
             reachy.turn_off_smoothly()
             time.sleep(0.5)
-            assert reachy.is_off, "Reachy is not turned off."
+            assert reachy.is_off()
 
     except Exception as e:
         print(f"An error occurred: {e}")
+        test_result = "FAILED"
         import traceback
 
         traceback.print_exc()
@@ -61,11 +64,11 @@ if __name__ == "__main__":
         goto_ids = reachy.goto_posture("default", wait=True)
         reachy.r_arm.gripper.open()
         reachy.l_arm.gripper.open()
-        reachy.cancel_all_gotos()
+        reachy.cancel_all_goto()
 
         print("Turning off Reachy")
         reachy.turn_off_smoothly()
 
         time.sleep(0.2)
 
-        exit("Exiting torques test")
+        exit("Exiting torques test with result: " + test_result)
